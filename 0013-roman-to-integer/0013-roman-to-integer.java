c3ee61df2ee02f39930d5b3f45666e1c2d0db88f
Jainil Desai <jayramdesai9657@gmail.com>
2025-09-07 19:39:24 +0530

    [LeetHub] Roman to Integer | Difficulty: Easy | Tags: Hash Table, Math, String

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int romanToInt(String s) {
+
+    int result = 0;
+    int prevValue = 0;
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        int curValue = getValue(s.charAt(i));
+        if (curValue < prevValue) {
+            result -= curValue;
+        } else {
+            result += curValue;
+        }
+        prevValue = curValue;
+    }
+
+    return result;
+}
+
+public static int getValue(char roman) {
+    switch (roman) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
+    }
+}
+
+    }
+
+
+    
