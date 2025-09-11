9fe04401c5a7b9f9eea7ecd47954af76d6e1d053
Jainil Desai <jayramdesai9657@gmail.com>
2025-09-11 19:46:42 +0530

    [LeetHub] Merge Two Sorted Lists | Difficulty: Easy | Tags: Linked List, Recursion

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,7 +7,6 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         tail = dummy
-
         while list1 and list2:
             if list1.val <= list2.val:
                 tail.next = list1
@@ -18,8 +17,6 @@ class Solution:
             tail = tail.next
         if list1:
             tail.next = list1
-        elif list2:
+        if list2:
             tail.next = list2
-        
-        return dummy.next
-
+        return dummy.next 
