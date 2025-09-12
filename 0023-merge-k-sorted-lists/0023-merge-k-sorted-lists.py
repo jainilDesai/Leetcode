cff5763a02efb4e63e62c296c93940aeeba70c48
Jainil Desai <jayramdesai9657@gmail.com>
2025-09-12 19:27:36 +0530

    [LeetHub] Merge k Sorted Lists | Difficulty: Hard | Tags: Linked List, Divide and Conquer, Heap (Priority Queue), Merge Sort

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -24,13 +24,11 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists and len(lists) == 0:
             return None
-        
         while len(lists) > 1:
             mergedLists = []
-
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
+                mergedLists.append(self.mergeList(l1,l2))
             lists = mergedLists
         return lists[0]
