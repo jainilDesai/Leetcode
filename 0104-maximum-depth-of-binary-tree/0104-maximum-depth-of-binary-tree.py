6719eb174bf98069361dca3ec39588006f67e83a
Jainil Desai <jayramdesai9657@gmail.com>
2025-09-18 19:53:40 +0530

    [LeetHub] Maximum Depth of Binary Tree | Difficulty: Easy | Tags: Tree, Depth-First Search, Breadth-First Search, Binary Tree

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,14 +6,6 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # dfs
-        stack = [[root, 1]]
-        res = 0
-
-        while stack:
-            node, depth = stack.pop()
-            if node:
-                res = max(res, depth)
-                stack.append([node.left, depth + 1])
-                stack.append([node.right, depth + 1])
-        return res
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
